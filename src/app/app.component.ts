5810a3b4a3c5bbd0648a68a4f14ca13a38ef90f0
a121 <shankar.pisal@codevian.com>
2020-05-06 12:33:09 +0530

    initial chages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tabs';
+  searchTerm;
+  tabs = [{
+    id : 1,
+    tabName : "super-admin",
+  },{
+    id : 2,
+    tabName : "recruiter",
+  },{
+    id : 3,
+    tabName : "employeer",
+  },{
+    id : 4,
+    tabName : "admin",
+  },{
+    id : 4,
+    tabName : "enterprise",
+  }];
+
+  clickedTab(tabName){
+    console.log("clickedTab : ",tabName);
+  }
+
+  selectValue(){
+    console.log("",this.searchTerm);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import {FormsModule} from "@angular/forms"
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabsModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
